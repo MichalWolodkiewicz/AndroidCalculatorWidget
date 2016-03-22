3e2d48beb9b1cf3acbeed9d4f4cf66d9e7cf0bd2
Michal <office.mikrosoft@gmail.com>
2016-03-22 21:04:25 +0100

    Changed expression evaluatro precision.

--- app/src/main/java/mikrosoft/widget/calc/ExpressionEvaluator.java
+++ app/src/main/java/mikrosoft/widget/calc/ExpressionEvaluator.java
@@ -2,10 +2,12 @@ package mikrosoft.widget.calc;
 
 import com.udojava.evalex.Expression;
 
+import java.math.RoundingMode;
+
 public class ExpressionEvaluator {
 
     public String evaluate(String expression) {
-        String result = new Expression(expression).eval().toPlainString();
+        String result = new Expression(expression).setPrecision(10).setRoundingMode(RoundingMode.UNNECESSARY).eval().toPlainString();
         if(result.matches("[0.]*")) {
             result = "0";
         }
