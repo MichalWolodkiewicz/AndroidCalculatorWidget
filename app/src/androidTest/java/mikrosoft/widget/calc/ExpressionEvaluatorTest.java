78262c1c525c82c7c76a9653467d3df911b0e0b4
MichalWolodkiewicz <office.mikrosoft@gmail.com>
2016-03-31 22:37:40 +0200

    Remove last sign when it is operator.

--- app/src/androidTest/java/mikrosoft/widget/calc/ExpressionEvaluatorTest.java
+++ app/src/androidTest/java/mikrosoft/widget/calc/ExpressionEvaluatorTest.java
@@ -7,17 +7,31 @@ import org.hamcrest.Matchers;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
-public class ExpressionEvaluatorTest extends TestCase{
+public class ExpressionEvaluatorTest extends TestCase {
 
     ExpressionEvaluator expressionEvaluator = new ExpressionEvaluator();
 
-    @Test
     public void testRemoveFractalPartWhenResultIsZero() {
         // when
         String result = expressionEvaluator.evaluate("1.2-1.2");
 
         // then
-        assertThat(result, Matchers.equalTo("0"));
+        assertThat(result, equalTo("0"));
+    }
+
+    private static final String[] OPERATOR_REMOVE_TEST_DATA = {
+            "100+", "100/", "100-", "100+"
+    };
+
+    public void testRemoveOperatorBeforeEvaluate() {
+        for (String testData : OPERATOR_REMOVE_TEST_DATA) {
+            // when
+            String result = expressionEvaluator.evaluate(testData);
+            // then
+            String expectedResult = testData.substring(0, testData.length()-1);
+            assertThat(result, equalTo(expectedResult));
+        }
     }
 }
--- app/src/main/java/mikrosoft/widget/calc/ExpressionEvaluator.java
+++ app/src/main/java/mikrosoft/widget/calc/ExpressionEvaluator.java
@@ -5,12 +5,23 @@ import com.udojava.evalex.Expression;
 public class ExpressionEvaluator {
 
     public static final int MAX_NUMBER_LENGTH = 10;
+    private static final String LAST_SIGN_IS_OPERATOR = "^.*[-/+*]$";
 
     public String evaluate(String expression) {
+        expression = cleanExpression(expression);
         String result = new Expression(expression).setPrecision(MAX_NUMBER_LENGTH).eval().toPlainString();
         if (result.matches("[0.]*")) {
             result = "0";
         }
         return result;
     }
+
+    private String cleanExpression(String expression) {
+        if(expression.matches(LAST_SIGN_IS_OPERATOR)) {
+            expression = expression.substring(0, expression.length()-1);
+        }
+        return expression;
+    }
+
+
 }
